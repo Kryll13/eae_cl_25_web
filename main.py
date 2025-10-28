1a849e45b6111e38656624375ceefc21e11e49e7
Kryll <kryll@free.fr>
2025-10-28 12:01:08 +0100

    variables et commentaires

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from eae-cl-25-web!")
+pi = 3.14159
+rayon = 2.2
 
+# équation de l'aire d'un cercle <- ceci est un commentaire
+aire = pi * (rayon**2)
+print(aire)
 
-if __name__ == "__main__":
-    main()
+# changer les valeurs du rayon <- un autre commentaire
+# utilisez les commentaires pour aider les autres à comprendre ce que vous faites dans le code
+
+rayon = rayon + 1
+print(aire)     # l'aire ne change pas
+aire = pi * (rayon**2)
+print(aire)
